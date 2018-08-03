b30dec32dfb1413fd9fa8b8ddc277492c1155a62
Dhiraj Sharma <dhiraj.smartmobe@gmail.com>
2018-08-03 12:52:11 +0545

    Create network service

---
+++ app/src/main/java/com/sharmadhiraj/androidpaginglibrarystepbystepimplementationguide/NetworkService.kt
@@ -0,0 +1,26 @@
+package com.sharmadhiraj.androidpaginglibrarystepbystepimplementationguide
+
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NetworkService {
+
+    @GET("/everything?q=sports&apiKey=aa67d8d98c8e4ad1b4f16dbd5f3be348")
+    fun getNews(@Query("page") page: Int, @Query("pageSize") pageSize: Int): Single<Response>
+
+    companion object {
+        fun getService(): NetworkService {
+            val retrofit = Retrofit.Builder()
+                    .baseUrl("https://newsapi.org/v2/")
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            return retrofit.create(NetworkService::class.java)
+        }
+    }
+
+}
